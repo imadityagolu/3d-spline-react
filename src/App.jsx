3a7191c7b73b173e5c19ff004b9cf889bce95f0a
Aditya Kumar <adityasng420.ak@gmail.com>
2025-08-07 23:51:41 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div style={{height: '98vh', width: '99vw'}}>
+        {/* text */}
+        {/* <Spline scene="https://prod.spline.design/CLkLqn4E1UCNFSce/scene.splinecode" /> */}
+
+        {/* adilanet */}
+        <Spline
+        scene="https://prod.spline.design/oQs2X0o9m8axKz9c/scene.splinecode" 
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
